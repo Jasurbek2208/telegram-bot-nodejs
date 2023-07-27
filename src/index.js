70214ec8e4af70d42673fea8b9dd8cb7b1855034
Jasur <jasurbekshomaqsudov2004@gmail.com>
2023-07-27 14:55:55 +0300

    add openai key but not works because not payed

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const { Telegraf, Markup } = require("telegraf");
+// const config = require("config");
+const { openai } = require("./openai")
+const { replyToUser, postPortfolioToServer } = require("./utils/functions");
+
+// const bot = new Telegraf(config.get("TELEGRAM_BOT_TOKEN"));
+const bot = new Telegraf(process.env("TELEGRAM_BOT_TOKEN"));
+
+bot.start((ctx) => {
+  // Using a more readable multiline array for the keyboard options
+  const keyboard = Markup.keyboard([
+    ["👋 Say Hello", "⚠️ Help!"],
+    ["🔗 Bot Commands"],
+  ]).resize();
+
+  ctx.reply("Assalamu Alaykum!", keyboard);
+});
+
+bot.on("text", async (ctx) => {
+  const userMessage = ctx.message.text;
+
+  switch (userMessage) {
+    case "👋 Say Hello":
+      await replyToUser(ctx, "Hello there!");
+      break;
+
+    case "⚠️ Help!":
+      await replyToUser(ctx, "Helping !");
+      break;
+
+    default:
+      try {
+        // Get the user ID as the user identifier
+        const user = ctx.message.from.id;
+        await openai.getChatToGPT(ctx, userMessage, user)
+      } catch (error) {
+        await replyToUser(ctx, `Error in sending text: \n${error}`);
+      }
+      break;
+  }
+});
+
+bot.on("photo", async (ctx) => {
+  const caption = ctx.message.caption || "";
+  const [title, project_link, github_link] = caption.split("§");
+  const img = ctx.message.photo[ctx.message.photo.length - 1].file_id;
+
+  const data = { title, img, project_link, github_link };
+
+  postPortfolioToServer(ctx, data)
+});
+
+bot.launch()
+  .then(() => {
+    console.log("Telegram bot is running.");
+  })
+  .catch((err) => {
+    console.error("Error launching Telegram bot:", err);
+  });
---
+++ src/utils/functions.js
@@ -0,0 +1,35 @@
+const axios = require("axios")
+// const config = require("config")
+
+class Generalfunctions {
+    // Helper function to send a reply to the user
+    async replyToUser(ctx, message, isHTML = false) {
+        try {
+            await ctx.reply(message, { parse_mode: isHTML ? 'HTML' : 'MarkdownV2' });
+        } catch (error) {
+            await ctx.reply(`Error in sending text: \n${error}`, { parse_mode: 'HTML' });
+        }
+    }
+
+
+    // Sending post to portfolio api server
+    async postPortfolioToServer(ctx, data) {
+        try {
+            // const res = await axios.post(`${config.get("BASE_BACKEND_API_URL")}/portfolio`, data, {
+            const res = await axios.post(`${process.env("BASE_BACKEND_API_URL")}/portfolio`, data, {
+                headers: {
+                    "Content-Type": "application/json",
+                    // Authorization: config.get('USER_BEARER_TOKEN'),
+                    Authorization: process.env('USER_BEARER_TOKEN'),
+                },
+            });
+            // await generalFunctions.replyToUser(ctx, res.data.message += `\n\nView this post:\n${config.get("BASE_BACKEND_API_URL")}/portfolios/${res.data.data.id}`);
+            await generalFunctions.replyToUser(ctx, res.data.message += `\n\nView this post:\n${process.env("BASE_BACKEND_API_URL")}/portfolios/${res.data.data.id}`);
+        } catch (error) {
+            await generalFunctions.replyToUser(ctx, `<b>Error: </b> \n${error.response.data.message}`);
+        }
+    }
+}
+
+const generalFunctions = new Generalfunctions();
+module.exports = generalFunctions
